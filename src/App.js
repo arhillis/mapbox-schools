a48066633d8e58a44d4b448cc3569b990c4a727f
Ann Hillis <andriellerhillis@gmail.com>
2019-08-05 13:47:52 -0500

    Added the basic sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Sidebar from './components/sidebar'
+
 function App() {
   return (
     <div className="App">
       App goes here.
-    
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+    constructor(){
+        super();
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="sidebar">
+                Sidebar goes here.
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
