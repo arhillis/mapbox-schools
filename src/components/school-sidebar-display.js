bc341c4fbda47b2e8d0a7c8d854015403a176cf1
Ann Hillis <andriellerhillis@gmail.com>
2019-08-16 14:10:44 -0500

    Added phone number, conditional rendering for city

--- src/components/school-sidebar-display.js
+++ src/components/school-sidebar-display.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class SchoolSidebarDisplay extends Component {
     render() {
-        const {properties: {campus, address: {street, zipCode}}} = this.props.school;
+        const {properties: {campus, address: {street, zipCode}, phoneNumber}} = this.props.school;
 
         return (
             <li>
                 <h3>{campus}</h3>
                 <p>{street}</p>
-                <p>Texarkana, TX {zipCode}</p>
+                <p>{zipCode === "75569" ? "Nash" : "Texarkana"}, TX {zipCode}</p>
+                <p>{phoneNumber}</p>
             </li>
         );
     }
