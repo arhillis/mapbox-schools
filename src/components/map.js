2548e1672e43ccbdc9df5019c6221b95e74df752
Ann Hillis <andriellerhillis@gmail.com>
2019-08-16 10:45:32 -0500

    Put the marker into its own component

--- src/components/map.js
+++ src/components/map.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
+import SchoolMapDisplay from './school-map-display';
 
 class Map extends Component {
     constructor(props){
         super(props)
-        this.state = {...this.props}
+        this.state = {...props}
     }
 
-
     render() {
-        const {API_KEY, viewport, schools} = this.props;
-
+        const {API_KEY, viewport} = this.state;
+        const {schools} = this.props;
+        
         return (
             <div className="map">
                 <ReactMapGL
@@ -19,21 +20,12 @@ class Map extends Component {
                 onViewportChange={(viewport) => this.setState({viewport})}
                 >
                 {schools.map((school) =>
-                    <Marker key={school.id} 
-                    latitude={school.geometry.coordinates[1]}
-                    longitude={school.geometry.coordinates[0]}
-                    
-                    >
-                    <img src="/graduation-hat.png" 
-                        alt="graduation hat" 
-                        className="marker-icon"
-                    />
-                    <span className="marker-label">
-                        {school.properties.campus}
-                    </span>
-                    </Marker>
+                    <SchoolMapDisplay school={school} key={school.id} />
                 )}
                 </ReactMapGL>
+                <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank"
+                    rel="noopener noreferrer"
+                >CC 3.0 BY</a></div>
             </div>
         );
     }
