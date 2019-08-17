e60597fbb2d394b44a0358e9f33b028bc3f6e029
Ann Hillis <andriellerhillis@gmail.com>
2019-08-17 09:21:30 -0500

    Updates

---
+++ precache-manifest.5d9df7de91fa70debccbbab6a944ef1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed046d3d259d2b47467e90824d9f39ff",
+    "url": "/mapbox-schools/index.html"
+  },
+  {
+    "revision": "c94000a3fa3fb1f057d2",
+    "url": "/mapbox-schools/static/css/main.a082e016.chunk.css"
+  },
+  {
+    "revision": "06baf3074c2e4b04d642",
+    "url": "/mapbox-schools/static/js/2.507a2848.chunk.js"
+  },
+  {
+    "revision": "c94000a3fa3fb1f057d2",
+    "url": "/mapbox-schools/static/js/main.ad5b7bda.chunk.js"
+  },
+  {
+    "revision": "088b8df5f80ad9cab7ff",
+    "url": "/mapbox-schools/static/js/runtime~main.5c2cde65.js"
+  }
+]);
